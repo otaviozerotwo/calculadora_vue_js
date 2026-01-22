87ea9a16cbf62b414834227011935a1a94b04807
Otavio <otavio.zerotwo@gmail.com>
2026-01-21 22:55:45 -0300

    feat: add estrutura inicial html

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,33 @@
 </script>
 
 <template>
-  
+  <div class="container">
+    <h1 class="text-center py-5">Calculadora Aritmética</h1>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="mb-3">
+          <input type="number" class=" w-100">
+        </div>
+        <div class="mb-3">
+          <select class="w-100">
+            <option value="">+</option>
+            <option value="">-</option>
+            <option value="">x</option>
+            <option value="">/</option>
+          </select>
+        </div>
+        <div class="mb-3">
+          <input type="number" class="w-100">
+        </div>
+        <div class="mb-3">
+          <span>=</span>
+        </div>
+        <div class="mb-3">
+          <input type="number" class="w-100">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
