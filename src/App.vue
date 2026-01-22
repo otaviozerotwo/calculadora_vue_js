4159adff5bef941ecb8eee8a20ee3d9abef061c7
otaviozerotwo <otavio.zerotwo@gmail.com>
2026-01-22 11:49:15 -0300

    style: add estilos iniciais

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,27 @@
   <div class="container">
     <h1 class="text-center py-5">Calculadora Aritmética</h1>
     <div class="row justify-content-center">
-      <div class="col-md-6">
+      <div class="col-md-4 col-sm-6 col-lg-4">
         <div class="mb-3">
-          <input type="number" class=" w-100">
+          <input type="number" class="w-100 rounded">
         </div>
-        <div class="mb-3">
-          <select class="w-100">
-            <option value="">+</option>
-            <option value="">-</option>
-            <option value="">x</option>
-            <option value="">/</option>
+        <div class="mb-3 d-flex justify-content-end">
+          <select class="w-25 rounded">
+            <option value="" class="symbol" disabled selected hidden>...</option>
+            <option value="" class="symbol text-center">+</option>
+            <option value="" class="symbol text-center">-</option>
+            <option value="" class="symbol text-center">x</option>
+            <option value="" class="symbol text-center">/</option>
           </select>
         </div>
         <div class="mb-3">
-          <input type="number" class="w-100">
+          <input type="number" class="w-100 rounded">
+        </div>
+        <div class="mb-3 d-flex justify-content-end">
+          <span class="symbol">=</span>
         </div>
         <div class="mb-3">
-          <span>=</span>
-        </div>
-        <div class="mb-3">
-          <input type="number" class="w-100">
+          <input type="number" class="w-100 rounded">
         </div>
       </div>
     </div>
@@ -33,5 +34,8 @@
 </template>
 
 <style scoped>
-
+  .symbol {
+    font-size: 48px;
+    font-weight: bold;
+  }
 </style>
